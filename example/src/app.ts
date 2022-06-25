17f8dfb07e8529d422d5648ddcb08d4858114200
Ivan Admaers <admaers@icloud.com>
2022-06-25 14:34:03 +0400

    migration to typescript

--- example/src/app.ts
+++ example/src/app.ts
@@ -1,6 +1,12 @@
-const path = require('path');
-const express = require('express');
-require('dotenv').config({ path: path.join(__dirname, '.env') });
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import type { Request, Response } from 'express';
+
+// Captcha Verifier
+import verifier from 'captcha-verifier';
+
+dotenv.config({ path: path.resolve('./.env') });
 
 // Env
 const {
@@ -12,9 +18,6 @@ const {
   RECAPTCHA_3_SECRET_KEY,
   HCAPTCHA_SECRET_KEY,
 } = process.env;
-
-// Captcha Verifier
-const verifier = require('../dist');
 
 verifier.config({
   reCaptchaV2SecretKey: RECAPTCHA_2_SECRET_KEY,
@@ -29,11 +32,11 @@ const app = express();
 app.use(express.json());
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.resolve('./src/views'));
+app.use(express.static(path.resolve('./src/public')));
 
 // Routes
-app.get('/', (req, res) =>
+app.get('/', (req: Request, res: Response) =>
   res.render('index.ejs', {
     reCaptcha2PublicKey: RECAPTCHA_2_PUBLIC_KEY,
     hCaptchaPublicKey: HCAPTCHA_PUBLIC_KEY,
@@ -42,7 +45,7 @@ app.get('/', (req, res) =>
 );
 
 // ReCaptcha 2
-app.post('/recaptcha2', async (req, res) => {
+app.post('/recaptcha2', async (req: Request, res: Response) => {
   const { token } = req.body;
 
   const [success, response] = await verifier.reCaptchaV2(token, req.ip);
@@ -51,7 +54,7 @@ app.post('/recaptcha2', async (req, res) => {
 });
 
 // hCaptcha
-app.post('/hcaptcha', async (req, res) => {
+app.post('/hcaptcha', async (req: Request, res: Response) => {
   const { token } = req.body;
 
   const [success, response] = await verifier.hCaptcha(
@@ -64,7 +67,7 @@ app.post('/hcaptcha', async (req, res) => {
 });
 
 // ReCaptcha 3
-app.post('/recaptcha3', async (req, res) => {
+app.post('/recaptcha3', async (req: Request, res: Response) => {
   const { token } = req.body;
 
   const [success, response] = await verifier.reCaptchaV3(token, req.ip);
